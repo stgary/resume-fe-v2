1e3147605ceb928c891a222b65374ba1c33e8968
stgary <sgary0@protonmail.com>
2020-10-11 20:08:43 -0400

    bg set

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Intro from './Intro';
 
 export default function App() {
   return (
-    <div>
-
+    <div className='app'>
+      <div className='bg-center'>
+        <Intro />
+      </div>
     </div>
   );
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faLinkedin,
+  faTwitterSquare,
+  faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+
 export default function Intro() {
   return (
-    <div>
-      
+    <div className='intro'>
+      <div className='social-media-icons'>
+        <a href='https://github.com/stgary'>
+          <FontAwesomeIcon className='cv-icon' icon={faGithubSquare} />
+        </a>
+        <a href='https://www.linkedin.com/in/sgary0'>
+          <FontAwesomeIcon className='cv-icon' icon={faLinkedin} />
+        </a>
+        <a href='https://twitter.com/explore'>
+          <FontAwesomeIcon className='cv-icon' icon={faTwitterSquare} />
+        </a>
+      </div>
     </div>
   )
 }
