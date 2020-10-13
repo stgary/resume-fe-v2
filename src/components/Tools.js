92b61206c567ff1c454e0cbd2c9be4576f2b9dbb
stgary <sgary0@protonmail.com>
2020-10-13 02:30:06 -0400

    init setup

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTools } from '@fortawesome/free-solid-svg-icons';
+
 export default function Tools() {
   return (
-    <div>
-      
+    <div className='tools'>
+      <div className='title'>
+        <p>Tools</p>
+        <p>of</p>
+        <p>Creation</p>
+      </div>
+      <div className='tool-icon'>
+        <FontAwesomeIcon className='icon' icon={faTools} />
+      </div>
+      <div className='nep'>
+        <p>Node.js</p>
+        <p>Express</p>
+        <p>PostgreSql</p>
+      </div>
+      <div className='rrl'>
+        <p>React</p>
+        <p>Redux</p>
+        <p>Less/Sass</p>
+      </div>
     </div>
   )
 }
