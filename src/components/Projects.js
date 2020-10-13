cb78e2bd4c493c2fd352e05ad110d45475602f7e
stgary <sgary0@protonmail.com>
2020-10-13 11:17:08 -0400

    init file structure

---
+++ src/components/Projects.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <div className='pro'>
+      <div className='pro-header'>
+        Projects
+      </div>
+      <div className='pro-container'>
+        <div className='pro-item'>
+          <p className='pro-title'></p>
+          <img className='pro-img' src={} alt='' />
+          <p className='pro-desc'></p>
+        </div>
+        <div className='pro-item'>
+          <p className='pro-title'></p>
+          <img className='pro-img' src={} alt='' />
+          <p className='pro-desc'></p>
+        </div>
+        <div className='pro-item'>
+          <p className='pro-title'></p>
+          <img className='pro-img' src={} alt='' />
+          <p className='pro-desc'></p>
+        </div>
+      </div>
+    </div>
+  )
+}
